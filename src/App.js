1365b270f30674e86816c9112ae268fa27450722
leoooooooo12138 <47234921+leoooooooo12138@users.noreply.github.com>
2021-12-07 01:04:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Web3 from "web3";
-import App1 from './src/App.js';
 // import CricketContractArtifact from "../../build/contracts/CricketContract.json";
 // import OwnableArtifact from "../../build/contracts/Ownable.json";
 // import SafemathArtifact from "../../build/contracts/Safemath.json";
@@ -970,4 +969,3 @@ window.addEventListener("load", function() {
   }
   App.start();
 });
-export default App1;
