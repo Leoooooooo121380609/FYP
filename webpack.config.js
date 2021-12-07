a8487f8bec674726d7889248e47791a1de8b775f
leoooooooo12138 <47234921+leoooooooo12138@users.noreply.github.com>
2021-12-07 15:04:26 +0800

    asdsadasd

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   //mode: 'development',
   mode: 'production',
-  entry: "./src/index.js",
+  entry: "./index.js",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new CopyWebpackPlugin([{ from: "./index.html", to: "index.html" }]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
 };
